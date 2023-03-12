1fa72f85d7430cd06a3014eba64fd481de862b13
Teemu Tynkkynen <teemu.tynkkynen@student.lut.fi>
2023-03-12 20:13:52 +0200

    Fix radio buttons for version selector

--- src/PTUI.WebApp/src/components/VersioSelector/VersionSelector.tsx
+++ src/PTUI.WebApp/src/components/VersioSelector/VersionSelector.tsx
@@ -6,34 +6,28 @@ import {changePreference} from "../../reducers/userSlice";
 import UserInterfaceHelpers from "../../helpers/UserInterfaceHelpers";
 
 function VersionSelector() {
-
     const { preferenceType } = useAppSelector((state) => state.user);
     const dispatch = useAppDispatch();
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    
     const [prefType, setPrefType] = useState(preferenceType);
     
     useEffect(() => {
-        (document.getElementById('preferenceTypeRadio' + preferenceType) as HTMLInputElement).checked = true;
-    }, [preferenceType]);
-    
-    useEffect(() => {
         const asyncWrapper = async () => {
+            await new Promise(f => setTimeout(f, 50));
             let userId: string = localStorage.getItem('userId') ?? "";
             let type = await UserInterfaceHelpers.getUserPreferenceFit(userId);
             setPrefType(Number(type));
         }
-        
         asyncWrapper()
-    }, [])
+    })
 
-    const changePreferenceType = async (type: number) => {
-        dispatch(changePreference(type));
-        await UserInterfaceHelpers.setUserPreferenceFit(UserInterfaceHelpers.getUserId(), type);
-        await UserInterfaceHelpers.setUserPreferencesFromDatabase(UserInterfaceHelpers.getUserId(), type)
-        setPrefType(type);
+    const changePreferenceType = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        let value = Number(e.currentTarget.value);
+        dispatch(changePreference(value));
+        await UserInterfaceHelpers.setUserPreferenceFit(UserInterfaceHelpers.getUserId(), value);
+        await UserInterfaceHelpers.setUserPreferencesFromDatabase(UserInterfaceHelpers.getUserId(),value)
+        setPrefType(value);
     }
-    
+
     return (
         <div id={style.versionSelectorDiv}>
             <h2>Select a version</h2>
@@ -41,22 +35,22 @@ function VersionSelector() {
                 <div id={style.versionSelectorFormDiv}>
                     <label htmlFor={'preferenceTypeRadio4'}>
                         <input id={'preferenceTypeRadio4'} name={'preferenceTypeRadio'} type={"radio"} value={4}
-                               onClick={() => changePreferenceType(4)}/>
+                               onChange={(e) => changePreferenceType(e)} checked={prefType === 4}/>
                         &nbsp;Default
                     </label>
                     <label htmlFor={'preferenceTypeRadio0'}>
                         <input id={'preferenceTypeRadio0'} name={'preferenceTypeRadio'} type={"radio"} value={0}
-                               onClick={() => changePreferenceType(0)}/>
+                               onChange={(e) => changePreferenceType(e)} checked={prefType === 0}/>
                         &nbsp;Version 1
                     </label>
                     <label htmlFor={'preferenceTypeRadio1'}>
                         <input id={'preferenceTypeRadio1'} name={'preferenceTypeRadio'} type={"radio"} value={1}
-                               onClick={() => changePreferenceType(1)}/>
+                               onChange={(e) => changePreferenceType(e)} checked={prefType === 1}/>
                         &nbsp;Version 2
                     </label>
                     <label htmlFor={'preferenceTypeRadio2'}>
                         <input id={'preferenceTypeRadio2'} name={'preferenceTypeRadio'} type={"radio"} value={2}
-                               onClick={() => changePreferenceType(2)}/>
+                               onChange={(e) => changePreferenceType(e)} checked={prefType === 2}/>
                         &nbsp;Version 3
                     </label>
                 </div>
